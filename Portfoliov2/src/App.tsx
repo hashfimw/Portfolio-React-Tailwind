2bc83e016c217e4f0956037320c4d783a5859e42
hashfimw <hashfimawarid@Hashfis.local>
2024-10-27 23:35:56 +0700

    PortfolioV2

---
+++ Portfoliov2/src/App.tsx
@@ -0,0 +1,18 @@
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+
+function App() {
+  return (
+    <div>
+      {/* Navbar */}
+      <Navbar />
+      {/* Content */}
+      <Homepage />
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Portfoliov2/src/components/Navbar.tsx
@@ -0,0 +1,79 @@
+import { useEffect, useState } from "react";
+
+function Navbar() {
+  const [show, setShow] = useState(false);
+  const [scroll, setScroll] = useState(false);
+  const handleClick = () => {
+    setShow(!show);
+  };
+
+  let menuActive = show ? "left-0" : "-left-full";
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      //if windownya di scroll secara vertikal lebih dari 5 maka setscroll true setshownya false, y=vertikal
+      if (window.scrollY > 5) {
+        // console.log("testing");
+        setScroll(true);
+        setShow(false);
+      } else {
+        setScroll(false); //tidak lebih dari 5 maka false
+      }
+    });
+  });
+
+  let scrollActive = scroll ? "py-6 bg-white shadow" : "py-4";
+  // pake tennary py 6 kalo true kalo false py nya 4
+  return (
+    <div className={`navbar fixed w-full transition-all ${scrollActive}`}>
+      <div className="container mx-auto px-4">
+        <div className="navbar-box flex items-center justify-between">
+          <div className="logo">
+            <h1 className="sm:text-2xl text-xl font-semibold">
+              HsfhfPortfolio.
+            </h1>
+          </div>
+          <ul
+            className={`flex lg:gap-12 md:static md:flex-row md:shadow-none md:bg-transparent md:w-auto md:h-full 
+            md:translate-y-0 md:text-black md:p-0 md:m-0 md:transition-none gap-8 fixed ${menuActive} top-1/2 -translate-y-1/2 flex-col px-8 py-6 rounded
+            shadow-lg shadow-slate-900 bg-slate-700 font-medium text-white transition-all`}
+          >
+            <li className="flex items-center gap-3">
+              <i className="ri-home-smile-line text-2xl md:hidden block"></i>
+              <a href="#home" className="font-regular opacity-75">
+                Home
+              </a>
+            </li>
+            <li className="flex items-center gap-3">
+              <i className="ri-user-smile-line text-2xl md:hidden block"></i>
+              <a href="#about" className="font-regular opacity-75">
+                About Me
+              </a>
+            </li>
+            <li className="flex items-center gap-3">
+              <i className="ri-mail-unread-line text-2xl md:hidden block"></i>
+              <a href="#contactme" className="font-regular opacity-75">
+                Contact Me
+              </a>
+            </li>
+          </ul>
+          <div className="Avatar flex items-center gap-2 mr-0 ml-10">
+            <a href="#about">
+              <img
+                src="../src/assets/avatar2.png"
+                alt="Avatar"
+                className="h-10 w-10 rounded-full border-2 border-slate-700 hover:animate-pulse"
+              />
+              <i
+                className="ri-menu-3-line text-3xl md:hidden block"
+                onClick={handleClick}
+              ></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
